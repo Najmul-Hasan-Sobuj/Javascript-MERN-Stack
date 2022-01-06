b27e5bdbed6388fbb7d3dafb69c562e509ce7750
Najmul-Hasan-Sobuj <najmulhasansobuj87@gmail.com>
2022-01-06 18:40:56 +0600

    Class 01 Assignment

---
+++ Class 01, Assignment/js/app.js
@@ -0,0 +1,20 @@
+// Make a devs info console project
+console.log(prompt('What is Your Name?'));
+console.log(prompt('What is Your Age?'));
+console.log(prompt('What is Your Designation?'));
+
+
+// Make a result marks project
+var Bangla = prompt('Input Your Bangla Subject Marks');
+var English = prompt('Input Your English Subject Marks');
+var Math = prompt('Input Your Math Subject Marks');
+var Chemistry = prompt('Input Your Chemistry Subject Marks');
+var Biology = prompt('Input Your Biology Subject Marks');
+
+alert('Bangla:'+Bangla+'\nEnglish:'+English+'\nMath:'+Math+'\nChemistry:'+Chemistry+'\nBiology:'+Biology);
+
+// Make a kobol project start
+function deleteFun() {
+    confirm('Are you sure you want to delete');
+}
+// Make a kobol project end
