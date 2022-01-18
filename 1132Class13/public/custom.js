c4aac03cd7b5ba1d9b8419b79cc381f9931bd8a1
Najmul-Hasan-Sobuj <najmulhasansobuj87@gmail.com>
2022-01-18 20:40:21 +0600

    Class 13 Assignment

--- 1132Class13/public/custom.js
+++ 1132Class13/public/custom.js
@@ -8,14 +8,18 @@ setInterval(() => {
 
 // Counter with Loader Start
 
-const start   = document.querySelector('#start');
-const stop    = document.querySelector('#stop');
-const reset   = document.querySelector('#reset');
-const counter = document.querySelector('#counter');
+const start        = document.querySelector('#start');
+const stop         = document.querySelector('#stop');
+const reset        = document.querySelector('#reset');
+const counter      = document.querySelector('#counter');
+const inputValue   = document.querySelector('#input');
+const inputBtn     = document.querySelector('#inputBtn');
+const progress_bar = document.querySelector('#progress_bar');
 
 counter.innerHTML = '▶';
 let counterTime;
 let counterValue = 0;
+
 start.addEventListener('click', () =>{
     counterTime =  setInterval(() => {
         counter.innerHTML = counterValue;
@@ -33,11 +37,29 @@ reset.addEventListener('click', () =>{
     clearInterval(counterTime);
 });
 
+let newCounter;
+let newSetInput;
+inputBtn.addEventListener('click', () =>{
+    newCounter  = inputValue.value;
+    
+    newSetInput = setInterval(() =>{
+        progress_bar.style.width = `${loader(inputValue.value,newCounter)}%`;
+        counter.innerHTML = newCounter;
+        if (newCounter == 0) {
+            clearInterval(newSetInput);
+            counter.innerHTML = '▶';
+        }
+        newCounter--;
+    },1000);
+});
 
-
-
-
-
-
-
+/**
+ * progress bar loader
+ * @param {*} startValue 
+ * @param {*} currentValue 
+ * @returns 
+ */
+function loader(startValue, currentValue) {
+    return (currentValue*100)/startValue;
+}
 // Counter with Loader End
