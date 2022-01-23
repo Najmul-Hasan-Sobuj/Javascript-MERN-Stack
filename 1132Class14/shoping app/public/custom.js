a92df0b1681ea9eff3e45658ce13b2c71e4d2ec8
Najmul-Hasan-Sobuj <najmulhasansobuj87@gmail.com>
2022-01-24 01:08:54 +0600

    Class 14 Assignment

--- 1132Class14/shoping app/public/custom.js
+++ 1132Class14/shoping app/public/custom.js
@@ -2,28 +2,28 @@
 const p_data = document.querySelector('.p_data');
 const p_form = document.getElementById('p_form');
 
+// const products = [{
+//         name: 'T-shart',
+//         price: 23,
+//         sale: 50,
+//         photo: 'https://www.oxfordshiremind.org.uk/app/uploads/2019/08/tshirt.png',
+//     },
+//     {
+//         name: 'T-shart',
+//         price: 23,
+//         sale: 50,
+//         photo: 'https://5.imimg.com/data5/FJ/GS/MY-2101395/bullet-crosshatch-t-shirt-500x500.jpg',
+//     },
+//     {
+//         name: 'T-shart',
+//         price: 23,
+//         sale: 50,
+//         photo: 'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1631028589-screen-shot-2021-09-07-at-11-29-13-am-1631028561.png?crop=0.829xw:0.843xh;0.0657xw,0.0783xh&resize=480:*',
+//     },
+// ];
 
-const products = [{
-        name: 'T-shart',
-        price: 23,
-        sale: 50,
-        photo: 'https://www.oxfordshiremind.org.uk/app/uploads/2019/08/tshirt.png',
-    },
-    {
-        name: 'T-shart',
-        price: 23,
-        sale: 50,
-        photo: 'https://5.imimg.com/data5/FJ/GS/MY-2101395/bullet-crosshatch-t-shirt-500x500.jpg',
-    },
-    {
-        name: 'T-shart',
-        price: 23,
-        sale: 50,
-        photo: 'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1631028589-screen-shot-2021-09-07-at-11-29-13-am-1631028561.png?crop=0.829xw:0.843xh;0.0657xw,0.0783xh&resize=480:*',
-    },
-];
+// dataSet('product', products);
 
-dataSet('product', products);
 /**
  * data set to localStorage
  * @param {*} key 
@@ -32,10 +32,10 @@ dataSet('product', products);
 function dataSet(key, value) {
     let productsDataSet = JSON.stringify(value);
     localStorage.setItem(key, productsDataSet);
+    return true;
 }
 
-
-let vari = dataget('product');
+// let vari = dataget('product');
 /**
  * data get from localStorage
  * @param {*} key 
@@ -52,27 +52,50 @@ p_form.addEventListener('submit', function(e) {
     let p_price   = this.querySelector('input[name = "p_price"]').value;
     let p_s_price = this.querySelector('input[name = "p_s_price"]').value;
     let p_img     = this.querySelector('input[name = "p_img"]').value;
-    console.log(p_name + p_price + p_s_price + p_img );
+    
+    let productArr;
+    if (dataget('product')) {
+        productArr = dataget('product');
+    }else{
+        productArr = [];
+    }
+    productArr.push({p_name: p_name, p_price: p_price,p_s_price: p_s_price,p_img: p_img,});
+    dataSet('product', productArr);
+    allProducts();
+
 });
 
-vari.forEach(element => {
-    p_data.innerHTML += `
-    <div class="col-md-4">
-        <div class="card m-2">
-            <img src="${element.photo}" class="card-img-top img-fluid" alt="">
-            <div class="card-body">
-                <div class="card-title">
-                    <h4>${element.name}</h4>
-                </div>
-                <span class="text-muted">$${element.price} <del>$${element.sale}</del> </span>
-                <div class="d-flex justify-content-end">
-                    <button type="submit" class="btn rounded-pill btn-primary">add to cart</button>
+allProducts();
+function allProducts() {
+    let allProducts = dataget('product');
+    let data = '';
+    allProducts.map(element => {
+        data += `
+        <div class="col-md-4">
+            <div class="card m-2">
+                <img src="${element.photo}" class="card-img-top img-fluid" alt="">
+                <div class="card-body">
+                    <div class="card-title">
+                        <h4>${element.name}</h4>
+                    </div>
+                    <span class="text-muted">$${element.price} <del>$${element.sale}</del> </span>
+                    <div class="d-flex justify-content-end">
+                        <button type="submit" class="btn rounded-pill btn-primary">add to cart</button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    `;
-});
+        `;
+    });
+    p_data.innerHTML += data;
+}
+
+
+
+
+
+
+
 
 
 
