e8dad40fbd4a6a538bf628f743be3d1e54a8b92d
Najmul-Hasan-Sobuj <najmulhasansobuj87@gmail.com>
2022-01-25 15:08:29 +0600

    Class 14 Assignment

--- 1132Class14/shoping app/public/custom.js
+++ 1132Class14/shoping app/public/custom.js
@@ -25,12 +25,12 @@ const p_data = document.querySelector('.p_data');
 p_form.addEventListener('submit', function (e) {
     e.preventDefault();
 
-    let p_name    = this.querySelector('input[name = "p_name"]');
-    let p_price   = this.querySelector('input[name = "p_price"]');
-    let p_s_price = this.querySelector('input[name = "p_s_price"]');
-    let p_img     = this.querySelector('input[name = "p_img"]');
-
+    let p_name    = this.querySelector('input[name = "p_name"]').value;
+    let p_price   = this.querySelector('input[name = "p_price"]').value;
+    let p_s_price = this.querySelector('input[name = "p_s_price"]').value;
+    let p_img     = this.querySelector('input[name = "p_img"]').value;
     let dataArray;
+
     if (getDataLs('products')) {
         dataArray = getDataLs('products');
     }else{
@@ -43,7 +43,6 @@ p_form.addEventListener('submit', function (e) {
         p_s_price: p_s_price,
         p_img    : p_img
     });
-    console.log(dataArray);
 
     setDataLs('products', dataArray);
     allProducts();
@@ -53,9 +52,9 @@ p_form.addEventListener('submit', function (e) {
 allProducts();
 function allProducts() {
     let allProducts = getDataLs('products');
-    let Empty = '';
+    let emptyStore = '';
     allProducts.forEach(element => {
-        Empty += `
+        emptyStore += `
         <div class="col-md-4">
             <div class="card m-2">
                 <img src="${element.p_img}" class="card-img-top img-fluid" alt="">
@@ -63,7 +62,7 @@ function allProducts() {
                     <div class="card-title">
                         <h4>${element.p_name}</h4>
                     </div>
-                    <span class="text-muted">$${element.p_price} <del>$${element.p_s_price}</del> </span>
+                    <span class="text-muted">${element.p_price} <del>${element.p_s_price}</del> </span>
                     <div class="d-flex justify-content-end">
                         <button type="submit" class="btn rounded-pill btn-primary">add to cart</button>
                     </div>
@@ -72,5 +71,5 @@ function allProducts() {
         </div>
         `;
     });
-    p_data.innerHTML = Empty;
+    p_data.innerHTML = emptyStore;
 }
