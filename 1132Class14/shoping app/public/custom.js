639caaa4c111a949ef0dacff8a2117067ad3e06f
Najmul-Hasan-Sobuj <najmulhasansobuj87@gmail.com>
2022-01-25 14:45:57 +0600

    Class 14 Assignment

--- 1132Class14/shoping app/public/custom.js
+++ 1132Class14/shoping app/public/custom.js
@@ -1,84 +1,69 @@
-// add to product project start
-const p_data = document.querySelector('.p_data');
-const p_form = document.getElementById('p_form');
-
-// const products = [{
-//         name: 'T-shart',
-//         price: 23,
-//         sale: 50,
-//         photo: 'https://www.oxfordshiremind.org.uk/app/uploads/2019/08/tshirt.png',
-//     },
-//     {
-//         name: 'T-shart',
-//         price: 23,
-//         sale: 50,
-//         photo: 'https://5.imimg.com/data5/FJ/GS/MY-2101395/bullet-crosshatch-t-shirt-500x500.jpg',
-//     },
-//     {
-//         name: 'T-shart',
-//         price: 23,
-//         sale: 50,
-//         photo: 'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1631028589-screen-shot-2021-09-07-at-11-29-13-am-1631028561.png?crop=0.829xw:0.843xh;0.0657xw,0.0783xh&resize=480:*',
-//     },
-// ];
-
-// dataSet('product', products);
-
+// function start
 /**
- * data set to localStorage
+ * set data to localStorage
  * @param {*} key 
  * @param {*} value 
  */
-function dataSet(key, value) {
-    let productsDataSet = JSON.stringify(value);
-    localStorage.setItem(key, productsDataSet);
+function setDataLs(key, value) {
+    let data = JSON.stringify(value);
+    localStorage.setItem(key, data);
     return true;
-}
-
-// let vari = dataget('product');
+} 
 /**
- * data get from localStorage
+ * get dala from localStorage
  * @param {*} key 
- * @returns 
  */
-function dataget(key) {
-    let productsDataGet = localStorage.getItem(key);
-    return productsDataGet ? JSON.parse(productsDataGet) : false;
-}
+function getDataLs(key) {
+    let data = localStorage.getItem(key);
+    return data ? JSON.parse(data) : false ;
+}  
+// function end  
+const p_form = document.getElementById('p_form');
+const p_data = document.querySelector('.p_data');
 
-p_form.addEventListener('submit', function(e) {
+
+p_form.addEventListener('submit', function (e) {
     e.preventDefault();
-    let p_name    = this.querySelector('input[name = "p_name"]').value;
-    let p_price   = this.querySelector('input[name = "p_price"]').value;
-    let p_s_price = this.querySelector('input[name = "p_s_price"]').value;
-    let p_img     = this.querySelector('input[name = "p_img"]').value;
-    
-    let productArr;
-    if (dataget('product')) {
-        productArr = dataget('product');
+
+    let p_name    = this.querySelector('input[name = "p_name"]');
+    let p_price   = this.querySelector('input[name = "p_price"]');
+    let p_s_price = this.querySelector('input[name = "p_s_price"]');
+    let p_img     = this.querySelector('input[name = "p_img"]');
+
+    let dataArray;
+    if (getDataLs('products')) {
+        dataArray = getDataLs('products');
     }else{
-        productArr = [];
+        dataArray = [];
     }
-    productArr.push({p_name: p_name, p_price: p_price,p_s_price: p_s_price,p_img: p_img,});
-    dataSet('product', productArr);
+
+    dataArray.push({
+        p_name   : p_name,
+        p_price  : p_price,
+        p_s_price: p_s_price,
+        p_img    : p_img
+    });
+    console.log(dataArray);
+
+    setDataLs('products', dataArray);
     allProducts();
 
 });
 
 allProducts();
 function allProducts() {
-    let allProducts = dataget('product');
-    let data = '';
-    allProducts.map(element => {
-        data += `
+    let allProducts = getDataLs('products');
+    let Empty = '';
+    allProducts.forEach(element => {
+        Empty += `
         <div class="col-md-4">
             <div class="card m-2">
-                <img src="${element.photo}" class="card-img-top img-fluid" alt="">
+                <img src="${element.p_img}" class="card-img-top img-fluid" alt="">
                 <div class="card-body">
                     <div class="card-title">
-                        <h4>${element.name}</h4>
+                        <h4>${element.p_name}</h4>
                     </div>
-                    <span class="text-muted">$${element.price} <del>$${element.sale}</del> </span>
+                    <span class="text-muted">$${element.p_price} <del>$${element.p_s_price}</del> </span>
                     <div class="d-flex justify-content-end">
                         <button type="submit" class="btn rounded-pill btn-primary">add to cart</button>
                     </div>
@@ -87,17 +72,5 @@ function allProducts() {
         </div>
         `;
     });
-    p_data.innerHTML += data;
+    p_data.innerHTML = Empty;
 }
-
-
-
-
-
-
-
-
-
-
-
-// add to product project end
