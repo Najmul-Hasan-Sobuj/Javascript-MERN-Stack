367530925ca857ec7da1976c32f47122c9250eed
Najmul-Hasan-Sobuj <najmulhasansobuj87@gmail.com>
2022-02-10 19:13:55 +0600

    Class 15 Assignment

--- 1132Class15/public/custom.js
+++ 1132Class15/public/custom.js
@@ -3,9 +3,9 @@ const skills = document.querySelectorAll('input[type="checkbox"]');
 const list_group = document.querySelector('.list-group');
 
 skills.forEach(element => {
-    element.addEventListener('change',function () {
+    element.addEventListener('change', function () {
         let allData = document.querySelectorAll('input[type="checkbox"]:checked');
-        
+
         $emptyArr = [];
         allData.forEach(data => {
             $emptyArr.push(data.value);
@@ -35,7 +35,7 @@ let finalRes = () => {
 
 let allCel = () => {
     all_value = [];
-    document.querySelector('.stackValue').innerHTML = 0 ;
+    document.querySelector('.stackValue').innerHTML = 0;
     document.querySelector('.finalValue').innerHTML = 0;
 }
 let back = () => {
@@ -43,12 +43,29 @@ let back = () => {
     document.querySelector('.stackValue').innerHTML = all_value.join('');
 }
 
-
-
-
-
-
-
-
-
 // calculator end
+
+// clock start
+const secends = document.querySelector('.s');
+const minutes = document.querySelector('.m');
+const hours = document.querySelector('.h');
+
+setInterval(() => {
+
+    let time = new Date();
+
+    let currentsecends = time.getSeconds();
+    let current_minutes = time.getMinutes();
+    let current_hours = time.gethours();
+
+    secends.style.transform = `rotate(${clockStick(60,currentsecends)}deg)`;
+    minutes.style.transform = `rotate(${clockStick(60,current_minutes)}deg)`;
+    hours.style.transform = `rotate(${clockStick(12,current_hours)}deg)`;
+
+}, 1000);
+
+function clockStick(time, current) {
+    return (300 * current) / time;
+}
+
+// clock end
